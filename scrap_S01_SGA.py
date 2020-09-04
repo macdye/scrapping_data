1eede7b8e50ae05b42e7a5f9229863327bb1b6f7
JulieCHARRIER <julie_charrier@hotmail.fr>
2020-09-04 14:39:20 +0200

    scrap atlantis s1 et 2

---
+++ scrap_S01_SGA.py
@@ -0,0 +1,49 @@
+import requests
+import bs4
+import re
+
+
+pages_episodes_S01_SGA = [
+    'https://www.stargate-fusion.com/atlantis/episodes/166/101_une-nouvelle-ere-1-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/167/102_une-nouvelle-ere-2-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/168/103_invulnerable.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/169/104_38-minutes.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/170/105_soupcons.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/171/106_la-fin-de-l-innocence.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/172/107_serum.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/174/108_apparences.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/173/109_retour-sur-terre.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/175/110_en-pleine-tempete-1-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/176/111_en-pleine-tempete-2-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/209/112_duel.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/212/113_virus.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/211/114_hors-d-atteinte.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/213/115_le-grand-sommeil.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/214/116_la-communaute-des-quinze.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/215/117_derniers-messages.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/218/118_sous-hypnose.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/216/119_assieges-1-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/217/120_assieges-2-2.html']
+data_S01_SGA = []
+for page_episode_S01_SGA in pages_episodes_S01_SGA :
+    ep_S01_SGA = []
+    response_S01_SGA = requests.get(page_episode_S01_SGA)
+    soup_S01_SGA = bs4.BeautifulSoup(response_S01_SGA.text, 'html.parser')
+    info_epi_S01_SGA = soup_S01_SGA.find_all("font")
+    for epi_S01_SGA in info_epi_S01_SGA :
+        ep_S01_SGA.append(epi_S01_SGA.next_sibling)
+    data_S01_SGA.append(ep_S01_SGA)
+#print(data_S01_SGA)
+
+episodes_S01_SGA = []
+for d_S01_SGA in data_S01_SGA :    
+    episode_S01_SGA = {
+        'titre' : d_S01_SGA[0],
+        'numero' : d_S01_SGA[1],
+        'audience_us' : d_S01_SGA[3],
+        'audience_fr' : d_S01_SGA[4],
+        'scenariste' : d_S01_SGA[5],
+        'realisateur' : d_S01_SGA[6]
+        }
+    episodes_S01_SGA.append(episode_S01_SGA)
+print(episodes_S01_SGA)
---
+++ scrap_S02_SGA.py
@@ -0,0 +1,49 @@
+import requests
+import bs4
+import re
+
+
+pages_episodes_S02_SGA = [
+    'https://www.stargate-fusion.com/atlantis/episodes/262/301_menace-sur-la-terre.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/263/302_transformation.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/265/303_irresistible.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/264/304_face-a-face.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/266/305_copies-conformes.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/280/306_le-monde-reel.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/281/307_interets-communs.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/282/308_la-guerre-des-genies.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/283/309_la-machine-infernale.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/284/310_exil-force-1-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/285/311_exil-force-2-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/288/312_le-chant-des-baleines.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/286/313_invincible.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/289/314_le-peril-de-la-sagesse.html'
+    'https://www.stargate-fusion.com/atlantis/episodes/290/315_les-jeux-sont-faits.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/291/316_ames-en-detresse.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/287/317_une-question-d-ethique.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/292/318_immersion.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/293/319_l-equilibre-parfait.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/294/320_nom-de-code-horizon.html']
+data_S02_SGA = []
+for page_episode_S02_SGA in pages_episodes_S02_SGA :
+    ep_S02_SGA = []
+    response_S02_SGA = requests.get(page_episode_S02_SGA)
+    soup_S02_SGA = bs4.BeautifulSoup(response_S02_SGA.text, 'html.parser')
+    info_epi_S02_SGA = soup_S02_SGA.find_all("font")
+    for epi_S02_SGA in info_epi_S02_SGA :
+        ep_S02_SGA.append(epi_S02_SGA.next_sibling)
+    data_S02_SGA.append(ep_S02_SGA)
+#print(data_S02_SGA)
+
+episodes_S02_SGA = []
+for d_S02_SGA in data_S02_SGA :    
+    episode_S02_SGA = {
+        'titre' : d_S02_SGA[0],
+        'numero' : d_S02_SGA[1],
+        'audience_us' : d_S02_SGA[3],
+        'audience_fr' : d_S02_SGA[4],
+        'scenariste' : d_S02_SGA[5],
+        'realisateur' : d_S02_SGA[6]
+        }
+    episodes_S02_SGA.append(episode_S02_SGA)
+print(episodes_S02_SGA)
