69d81538bed3654efffd622b2f9a76ff8078af2a
JulieCHARRIER <julie_charrier@hotmail.fr>
2020-09-04 15:56:04 +0200

    harmonisation

--- scrap_S02_SGA.py
+++ scrap_S02_SGA.py
@@ -3,26 +3,26 @@ import bs4
 import re
 
 pages_episodes_S02_SGA = [
-    'https://www.stargate-fusion.com/atlantis/episodes/262/301_menace-sur-la-terre.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/263/302_transformation.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/265/303_irresistible.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/264/304_face-a-face.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/266/305_copies-conformes.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/280/306_le-monde-reel.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/281/307_interets-communs.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/282/308_la-guerre-des-genies.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/283/309_la-machine-infernale.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/284/310_exil-force-1-2.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/285/311_exil-force-2-2.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/288/312_le-chant-des-baleines.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/286/313_invincible.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/289/314_le-peril-de-la-sagesse.html'
-    'https://www.stargate-fusion.com/atlantis/episodes/290/315_les-jeux-sont-faits.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/291/316_ames-en-detresse.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/287/317_une-question-d-ethique.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/292/318_immersion.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/293/319_l-equilibre-parfait.html',
-    'https://www.stargate-fusion.com/atlantis/episodes/294/320_nom-de-code-horizon.html']
+    'https://www.stargate-fusion.com/atlantis/episodes/222/201_sous-le-feu-de-l-ennemi.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/231/202_i-a.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/220/203_chasse-a-l-homme.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/227/204_a-corps-perdu.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/237/205_les-condamnes.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/221/206_l-experience-interdite.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/239/207_instinct.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/238/208_mutation.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/230/209_l-aurore.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/251/210_l-union-fait-la-force-1-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/252/211_l-union-fait-la-force-2-2.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/232/212_tempus-fugit.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/242/213_masse-critique.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/253/214_l-ivresse-des-profondeurs.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/254/215_la-tour.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/256/216_possedes.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/257/217_coup-d-etat.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/255/218_traitement-de-choc.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/259/219_inferno.html',
+    'https://www.stargate-fusion.com/atlantis/episodes/260/220_les-allies.html']
 data_S02_SGA = []
 for page_episode_S02_SGA in pages_episodes_S02_SGA :
     ep_S02_SGA = []
@@ -47,3 +47,4 @@ for d_S02_SGA in data_S02_SGA :
         }
     episodes_S02_SGA.append(episode_S02_SGA)
 print(episodes_S02_SGA)
+
---
+++ scrap_S08_SG1.py
@@ -0,0 +1,49 @@
+import requests
+import bs4
+import re
+
+pages_episodes_S08_SG1 = [
+    'https://www.stargate-fusion.com/sg1/episodes/154/801_mesalliance-1-2.html',
+    'https://www.stargate-fusion.com/sg1/episodes/155/802_mesalliance-2-2.html',
+    'https://www.stargate-fusion.com/sg1/episodes/156/803_quarantaine.html',
+    'https://www.stargate-fusion.com/sg1/episodes/157/804_heure-h.html',
+    'https://www.stargate-fusion.com/sg1/episodes/158/805_le-feu-aux-poudres.html',
+    'https://www.stargate-fusion.com/sg1/episodes/159/806_avatar.html',
+    'https://www.stargate-fusion.com/sg1/episodes/161/807_monde-cruel.html',
+    'https://www.stargate-fusion.com/sg1/episodes/160/808_aux-yeux-du-monde.html',
+    'https://www.stargate-fusion.com/sg1/episodes/162/809_discordes.html',
+    'https://www.stargate-fusion.com/sg1/episodes/163/810_sans-pitie.html',
+    'https://www.stargate-fusion.com/sg1/episodes/219/811_vulnerable.html',
+    'https://www.stargate-fusion.com/sg1/episodes/164/812_en-detresse.html',
+    'https://www.stargate-fusion.com/sg1/episodes/165/813_une-vieille-connaissance.html',
+    'https://www.stargate-fusion.com/sg1/episodes/202/814_alerte-maximum.html',
+    'https://www.stargate-fusion.com/sg1/episodes/206/815_rien-a-perdre.html',
+    'https://www.stargate-fusion.com/sg1/episodes/203/816_la-derniere-chance-1-2.html',
+    'https://www.stargate-fusion.com/sg1/episodes/204/817_la-derniere-chance-2-2.html',
+    'https://www.stargate-fusion.com/sg1/episodes/205/818_pour-la-vie.html',
+    'https://www.stargate-fusion.com/sg1/episodes/207/819_retour-vers-le-futur-1-2.html',
+    'https://www.stargate-fusion.com/sg1/episodes/208/820_retour-vers-le-futur-2-2.html']
+data_S08_SG1 = []
+for page_episode_S08_SG1 in pages_episodes_S08_SG1 :
+    ep_S08_SG1 = []
+    response_S08_SG1 = requests.get(page_episode_S08_SG1)
+    soup_S08_SG1 = bs4.BeautifulSoup(response_S08_SG1.text, 'html.parser')
+    info_epi_S08_SG1 = soup_S08_SG1.find_all("font")
+    for epi_S08_SG1 in info_epi_S08_SG1 :
+        ep_S08_SG1.append(epi_S08_SG1.next_sibling)
+    data_S08_SG1.append(ep_S08_SG1)
+#print(data_S08_SG1)
+
+episodes_S08_SG1 = []
+for d_S08_SG1 in data_S08_SG1 :    
+    episode_S08_SG1 = {
+        'titre' : d_S08_SG1[0],
+        'numero' : d_S08_SG1[1],
+        'date de diffusion' : d_S08_SG1[2],
+        'audience_us' : d_S08_SG1[3],
+        'audience_fr' : d_S08_SG1[4],
+        'scenariste' : d_S08_SG1[5],
+        'realisateur' : d_S08_SG1[6]
+        }
+    episodes_S08_SG1.append(episode_S08_SG1)
+print(episodes_S08_SG1)
